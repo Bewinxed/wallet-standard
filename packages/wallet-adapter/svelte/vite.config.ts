046da7be7c63a9f43a3d01a142323236d28918d6
Bewinxed <bewinxed@gmail.com>
2024-08-10 12:52:55 +0000

    migrate from react to svelte 5

---
+++ packages/wallet-adapter/svelte/vite.config.ts
@@ -0,0 +1,13 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [svelte()],
+    build: {
+        lib: {
+            name: '@solana/wallet-standard-wallet-adapter-svelte',
+            entry: 'src/lib/index.ts',
+            fileName: 'index',
+        },
+    },
+});
